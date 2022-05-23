9dded7062d9d5fa77476c0a0508ad18b08907326
Lukas Zeller <lukas.zeller@nexenio.com>
2022-05-23 14:47:22 +0200

    :sparkles: add helpers for challenge 1

--- Playground.js
+++ Playground.js
@@ -19,4 +19,19 @@ function bufferToHex(buffer) { return buffer.toString('hex') }
 function bufferToBase64(buffer) { return buffer.toString('base64') }
 
 
+// XOR
+function xor(hex1, hex2) {
+  const buf1 = Buffer.from(hex1, 'hex');
+  const buf2 = Buffer.from(hex2, 'hex');
+  const bufResult = buf1.map((b, i) => b ^ buf2[i]);
+  return bufferToHex(bufResult);
+}
+
+// Ciphertexts
+// adapted from https://cryptopals.com/sets/1/challenges/3
+const CHALLENGE_1_A = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+// adapted from https://id0-rsa.pub/problem/32/
+const CHALLENGE_1_B = "ZNKIGKYGXIOVNKXOYGXKGRREURJIOVNKXCNOINOYXKGRRECKGQOSTUZYAXKNUCURJHKIGAYKOSZUURGFEZURUUQGZZNKCOQOVGMKGZZNKSUSKTZHAZOLOMAXKOZYMUZZUHKGZRKGYZROQKLOLZEEKGXYURJUXCNGZKBKXBGPJADLIVBAYKZNUYKRGYZZKTINGXGIZKXYGYZNKYURAZOUT"
+
+
 console.log(asciiToHex("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"))
